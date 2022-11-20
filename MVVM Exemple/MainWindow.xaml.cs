cb20a89fac33839f13008291c56da3b9534c3400
POMXARK <1997pom@gmail.com>
2022-11-20 20:41:02 +1000

    Add project files.

---
+++ MVVM Exemple/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace MVVM
+{
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// Логика mvvm.
+        /// в главном методе формы.
+        /// создать обьект DataContext = new ViewModel();
+        /// Реализующий интерфейс INotifyPropertyChanged
+        /// 
+        /// Неважн где описанна логика , если класс реализует INotifyPropertyChanged, 
+        /// то можно через свойства обновлять интерфейс через XAML Binding -> PropertyChanged // OnPropertyChanged("{Xaml Binding}")
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // привязаная левая часть к <StackPanel Grid.Column="1" DataContext="{Binding SelectedPhone}">
+            // можно разграничивать блоки приложение, таким образом
+            DataContext = new ApplicationViewModel();
+        }
+    }
+}
---
+++ MVVMExemple2/MainVM.cs
@@ -0,0 +1,45 @@
+﻿
+using System.ComponentModel;
+
+
+namespace MVVMExemple2
+{
+    public class MainVM : INotifyPropertyChanged
+    {
+        private int _number1;
+        public int Number1
+        {
+            get { return _number1; }
+            set
+            {
+                _number1 = value;
+                OnPropertyChanged("Number3"); // уведомление View о том, что изменилась сумма
+            }
+        }
+
+        private int _number2;
+        public int Number2
+        {
+            get { return _number2; }
+            set { _number2 = value; OnPropertyChanged("Number3"); }
+        }
+
+        //свойство только для чтения, оно считывается View каждый раз, когда обновляется Number1 или Number2
+        public int Number3
+        {
+            get
+            {
+                return Number1 + Number2;
+            }
+        }
+
+        /// <summary>
+        /// Магия MVVM, обработчик события, используется при вызове свойств
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
